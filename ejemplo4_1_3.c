a7a1d52bdf58a9f33528524aaf449a74e339bb33
JulianD01-ux <dabidjulian92@gmail.com>
2025-10-29 22:53:19 -0500

    Refactor variable names and enhance comments
    Updated variable names and improved comments for clarity.

--- ejemplo4_1_3.c
+++ ejemplo4_1_3.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    char letras[26], letra;
+    char abecedario[26];  // Arreglo que almacenará las letras del abecedario
+    char letra;           // Variable para recorrer las letras
 
+    // Llenar el arreglo con las letras de la 'a' a la 'z'
     for (letra = 'a'; letra <= 'z'; letra++)
     {
-        letras[letra - 'a'] = letra;
+        abecedario[letra - 'a'] = letra;
     }
 
+    // Mostrar las letras del abecedario en orden inverso (de 'z' a 'b')
+    printf("Letras del abecedario en orden inverso:\n");
     for (letra = 'z'; letra > 'a'; letra--)
     {
-        printf("%c\n", letras[letra - 'a']);
+        printf("%c\n", abecedario[letra - 'a']);
     }
 
-    printf("%c", letras['g' - 'a']);
-    printf("%c", letras['r' - 'a']);
-    printf("%c", letras['e' - 'a']);
-    printf("%c", letras['a' - 'a']);
-    printf("%c\n", letras['t' - 'a']);
+    // Imprimir la palabra "great" utilizando el arreglo
+    printf("\nPalabra formada: ");
+    printf("%c", abecedario['g' - 'a']);
+    printf("%c", abecedario['r' - 'a']);
+    printf("%c", abecedario['e' - 'a']);
+    printf("%c", abecedario['a' - 'a']);
+    printf("%c\n", abecedario['t' - 'a']);
 
     return 0;
 }
