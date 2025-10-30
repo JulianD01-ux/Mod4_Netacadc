41534465604fa143b2797bc44cf94ed290605b50
JulianD01-ux <dabidjulian92@gmail.com>
2025-10-29 22:51:57 -0500

    Update ejemplo4_1_2.c

--- ejemplo4_1_2.c
+++ ejemplo4_1_2.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    int serieFibo[10], i;
+    int serieFibo[10];   // Arreglo para almacenar los 10 primeros números de Fibonacci
+    int indice;          // Variable de control para los bucles
+
+    // Inicializar los dos primeros valores de la serie
     serieFibo[0] = 1;
     serieFibo[1] = 1;
 
-    for (i = 2; i < 10; i++)
+    // Calcular el resto de la serie
+    for (indice = 2; indice < 10; indice++)
     {
-        serieFibo[i] = serieFibo[i - 1] + serieFibo[i - 2];
+        serieFibo[indice] = serieFibo[indice - 1] + serieFibo[indice - 2];
     }
 
-
-    for (i = 0; i < 10; i++)
+    // Imprimir todos los valores de la serie
+    printf("Serie completa de Fibonacci:\n");
+    for (indice = 0; indice < 10; indice++)
     {
-        printf("%d\n", serieFibo[i]);
+        printf("%d\n", serieFibo[indice]);
     }
 
-    
-    for (i = 0; i < 10; i += 2)
+    // Imprimir solo los elementos en posiciones pares (0, 2, 4, ...)
+    printf("\nElementos en posiciones pares:\n");
+    for (indice = 0; indice < 10; indice += 2)
     {
-        printf("%d\n", serieFibo[i]);
+        printf("%d\n", serieFibo[indice]);
     }
 
-    
-    for (i = 1; i < 10; i += 2)
+    // Imprimir solo los elementos en posiciones impares (1, 3, 5, ...)
+    printf("\nElementos en posiciones impares:\n");
+    for (indice = 1; indice < 10; indice += 2)
     {
-        printf("%d\n", serieFibo[i]);
+        printf("%d\n", serieFibo[indice]);
     }
 
     return 0;
